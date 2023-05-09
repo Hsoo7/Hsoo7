ad24a3c0fe4231bb1470a24133fb2e9e2746030c
Hsoo7 <132774924+Hsoo7@users.noreply.github.com>
2023-05-09 03:48:23 +0300

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import logging
 import threading
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+From sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy import Column, PickleType, UnicodeText, distinct, func
 BASE = declarative_base()
 SESSION = start()
